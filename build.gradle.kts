cf18351fb50a0d2ed80381f096a3f08aac130087
bossm0n5t3r <bossm0n5t3r@gmail.com>
2024-12-29 23:41:00 +0900

    feat: Add dependencies
    - Add Content negotiation and serialization dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.serialization)
     alias(libs.plugins.ktor)
     alias(libs.plugins.ktlint)
 }
@@ -23,6 +24,9 @@ dependencies {
     implementation(libs.ktor.server.netty)
     implementation(libs.logback.classic)
     implementation(libs.ktor.server.config.yaml)
+    implementation(libs.ktor.serialization.kotlinx.json)
+    implementation(libs.ktor.server.content.negotiation)
+
     testImplementation(libs.ktor.server.test.host)
     testImplementation(libs.kotlin.test.junit)
 }
