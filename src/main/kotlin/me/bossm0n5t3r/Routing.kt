aa411e5fa835e89997aad71d2046ed558bdf3cca
bossm0n5t3r <bossm0n5t3r@gmail.com>
2025-01-01 00:35:00 +0900

    feat: Add POST /tasks API

--- src/main/kotlin/me/bossm0n5t3r/Routing.kt
+++ src/main/kotlin/me/bossm0n5t3r/Routing.kt
@@ -1,14 +1,18 @@
 package me.bossm0n5t3r
 
 import io.ktor.http.HttpStatusCode
+import io.ktor.serialization.JsonConvertException
 import io.ktor.server.application.Application
 import io.ktor.server.http.content.staticResources
+import io.ktor.server.request.receive
 import io.ktor.server.response.respond
 import io.ktor.server.response.respondText
 import io.ktor.server.routing.get
+import io.ktor.server.routing.post
 import io.ktor.server.routing.route
 import io.ktor.server.routing.routing
 import me.bossm0n5t3r.model.Priority
+import me.bossm0n5t3r.model.Task
 import me.bossm0n5t3r.model.TaskRepository
 
 fun Application.configureRouting() {
@@ -39,6 +43,7 @@ fun Application.configureRouting() {
                 }
                 call.respond(task)
             }
+
             get("/byPriority/{priority}") {
                 val priorityAsText = call.parameters["priority"]
                 if (priorityAsText == null) {
@@ -55,6 +60,18 @@ fun Application.configureRouting() {
                     }
                     call.respond(tasks)
                 } catch (ex: IllegalArgumentException) {
+                    call.respond(HttpStatusCode.BadRequest)
+                }
+            }
+
+            post {
+                try {
+                    val task = call.receive<Task>()
+                    TaskRepository.addTask(task)
+                    call.respond(HttpStatusCode.Created)
+                } catch (ex: IllegalStateException) {
+                    call.respond(HttpStatusCode.BadRequest)
+                } catch (ex: JsonConvertException) {
                     call.respond(HttpStatusCode.BadRequest)
                 }
             }
