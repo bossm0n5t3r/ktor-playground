9287da32e141bf9389ea5fae749b4ea998a9b9e0
bossm0n5t3r <bossm0n5t3r@gmail.com>
2024-12-31 23:45:00 +0900

    feat: Add GET /tasks API

--- src/main/kotlin/me/bossm0n5t3r/Routing.kt
+++ src/main/kotlin/me/bossm0n5t3r/Routing.kt
@@ -1,14 +1,31 @@
 package me.bossm0n5t3r
 
 import io.ktor.server.application.Application
+import io.ktor.server.http.content.staticResources
+import io.ktor.server.response.respond
 import io.ktor.server.response.respondText
 import io.ktor.server.routing.get
 import io.ktor.server.routing.routing
+import me.bossm0n5t3r.model.Priority
+import me.bossm0n5t3r.model.Task
 
 fun Application.configureRouting() {
     routing {
+        staticResources("static", "static")
+
         get("/") {
             call.respondText("Hello World!")
+        }
+
+        get("/tasks") {
+            call.respond(
+                listOf(
+                    Task("cleaning", "Clean the house", Priority.Low),
+                    Task("gardening", "Mow the lawn", Priority.Medium),
+                    Task("shopping", "Buy the groceries", Priority.High),
+                    Task("painting", "Paint the fence", Priority.Medium),
+                ),
+            )
         }
     }
 }
