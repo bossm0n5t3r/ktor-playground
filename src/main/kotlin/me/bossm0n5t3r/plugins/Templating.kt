3102433f9d3e529f10831881c6b6b4dc97ff3035
bossm0n5t3r <bossm0n5t3r@gmail.com>
2025-01-02 03:55:30 +0900

    feat: Update plugins/Templating.kt

--- src/main/kotlin/me/bossm0n5t3r/plugins/Templating.kt
+++ src/main/kotlin/me/bossm0n5t3r/plugins/Templating.kt
@@ -3,13 +3,16 @@ package me.bossm0n5t3r.plugins
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
+import io.ktor.server.request.receiveParameters
 import io.ktor.server.response.respond
 import io.ktor.server.routing.get
+import io.ktor.server.routing.post
 import io.ktor.server.routing.route
 import io.ktor.server.routing.routing
 import io.ktor.server.thymeleaf.Thymeleaf
 import io.ktor.server.thymeleaf.ThymeleafContent
 import me.bossm0n5t3r.model.Priority
+import me.bossm0n5t3r.model.Task
 import me.bossm0n5t3r.model.TaskRepository
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver
 
@@ -32,6 +35,7 @@ fun Application.configureTemplating() {
                     ThymeleafContent("all-tasks", mapOf("tasks" to tasks)),
                 )
             }
+
             get("/byName") {
                 val name = call.request.queryParameters["name"]
                 if (name == null) {
@@ -47,6 +51,7 @@ fun Application.configureTemplating() {
                     ThymeleafContent("single-task", mapOf("task" to task)),
                 )
             }
+
             get("/byPriority") {
                 val priorityAsText = call.request.queryParameters["priority"]
                 if (priorityAsText == null) {
@@ -68,6 +73,38 @@ fun Application.configureTemplating() {
                         )
                     call.respond(ThymeleafContent("tasks-by-priority", data))
                 } catch (ex: IllegalArgumentException) {
+                    call.respond(HttpStatusCode.BadRequest)
+                }
+            }
+
+            post {
+                val formContent = call.receiveParameters()
+                val params =
+                    Triple(
+                        formContent["name"] ?: "",
+                        formContent["description"] ?: "",
+                        formContent["priority"] ?: "",
+                    )
+                if (params.toList().any { it.isEmpty() }) {
+                    call.respond(HttpStatusCode.BadRequest)
+                    return@post
+                }
+                try {
+                    val priority = Priority.valueOf(params.third)
+                    TaskRepository.addTask(
+                        Task(
+                            params.first,
+                            params.second,
+                            priority,
+                        ),
+                    )
+                    val tasks = TaskRepository.allTasks()
+                    call.respond(
+                        ThymeleafContent("all-tasks", mapOf("tasks" to tasks)),
+                    )
+                } catch (ex: IllegalArgumentException) {
+                    call.respond(HttpStatusCode.BadRequest)
+                } catch (ex: IllegalStateException) {
                     call.respond(HttpStatusCode.BadRequest)
                 }
             }
