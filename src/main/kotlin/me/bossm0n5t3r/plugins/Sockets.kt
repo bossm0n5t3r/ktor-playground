27920e619ac8103a5ab0581644f969f3e3b2f598
bossm0n5t3r <bossm0n5t3r@gmail.com>
2025-01-08 02:40:00 +0900

    feat: Update plugins/Sockets.kt

--- src/main/kotlin/me/bossm0n5t3r/plugins/Sockets.kt
+++ src/main/kotlin/me/bossm0n5t3r/plugins/Sockets.kt
@@ -4,8 +4,11 @@ import io.ktor.serialization.kotlinx.KotlinxWebsocketSerializationConverter
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.ktor.server.routing.routing
+import io.ktor.server.websocket.DefaultWebSocketServerSession
+import io.ktor.server.websocket.WebSocketServerSession
 import io.ktor.server.websocket.WebSockets
 import io.ktor.server.websocket.pingPeriod
+import io.ktor.server.websocket.receiveDeserialized
 import io.ktor.server.websocket.sendSerialized
 import io.ktor.server.websocket.timeout
 import io.ktor.server.websocket.webSocket
@@ -13,8 +16,9 @@ import io.ktor.websocket.CloseReason
 import io.ktor.websocket.close
 import kotlinx.coroutines.delay
 import kotlinx.serialization.json.Json
-import me.bossm0n5t3r.model.Priority
 import me.bossm0n5t3r.model.Task
+import me.bossm0n5t3r.model.TaskRepository
+import java.util.Collections
 import kotlin.time.Duration.Companion.seconds
 
 fun Application.configureSockets() {
@@ -27,21 +31,32 @@ fun Application.configureSockets() {
     }
 
     routing {
+        val sessions =
+            Collections.synchronizedList<WebSocketServerSession>(ArrayList())
+
         webSocket("/websocket/tasks") {
-            val tasks =
-                listOf(
-                    Task("cleaning", "Clean the house", Priority.Low),
-                    Task("gardening", "Mow the lawn", Priority.Medium),
-                    Task("shopping", "Buy the groceries", Priority.High),
-                    Task("painting", "Paint the fence", Priority.Medium),
-                )
-
-            for (task in tasks) {
-                sendSerialized(task)
-                delay(1000)
-            }
-
+            sendAllTasks()
             close(CloseReason(CloseReason.Codes.NORMAL, "All done"))
         }
+
+        webSocket("/websocket/tasks2") {
+            sessions.add(this)
+            sendAllTasks()
+
+            while (true) {
+                val newTask = receiveDeserialized<Task>()
+                TaskRepository.addTask(newTask)
+                for (session in sessions) {
+                    session.sendSerialized(newTask)
+                }
+            }
+        }
+    }
+}
+
+private suspend fun DefaultWebSocketServerSession.sendAllTasks() {
+    for (task in TaskRepository.allTasks()) {
+        sendSerialized(task)
+        delay(1000)
     }
 }
