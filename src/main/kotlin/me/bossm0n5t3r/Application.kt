9fbfd633955df2155dde8fafc871713c2dc11bbe
bossm0n5t3r <bossm0n5t3r@gmail.com>
2025-01-19 04:24:10 +0900

    feat: Update Application.kt

--- src/main/kotlin/me/bossm0n5t3r/Application.kt
+++ src/main/kotlin/me/bossm0n5t3r/Application.kt
@@ -3,6 +3,7 @@ package me.bossm0n5t3r
 import io.ktor.server.application.Application
 import io.ktor.server.netty.EngineMain
 import me.bossm0n5t3r.model.FakeTaskRepository
+import me.bossm0n5t3r.plugins.configureDatabases
 import me.bossm0n5t3r.plugins.configureRouting
 import me.bossm0n5t3r.plugins.configureSerialization
 import me.bossm0n5t3r.plugins.configureSockets
@@ -19,4 +20,5 @@ fun Application.module() {
     configureSerialization()
     configureTemplating(repository)
     configureSockets(repository)
+    configureDatabases()
 }
